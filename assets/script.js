0c57f5f7614db59983e354d1a825c61a1b236739
Anthony Pileggi <adpileggi@gmail.com>
2024-01-26 16:25:21 -0500

    added for loop to get temps for 5 day forcast

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,28 @@ var citySearchEl = document.querySelector('#city-input')
 
 var searchBtn = document.querySelector('#search-btn')
 
+var apiUrl;
 
+searchBtn.addEventListener('click', getSearchHandler);
 
-searchBtn.addEventListener('click', getSearchWeather);
-
-
-function getSearchWeather(event) {
+function getSearchHandler(event) {
+    // find a way to remove all spaces?
     var findCity = citySearchEl.value.trim();
-    var makeUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + findCity + '&appid=ac772410ddc838ce4708a6abf9efd783';
-    console.log(makeUrl);
-}
+    apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + findCity + '&appid=ac772410ddc838ce4708a6abf9efd783&units=imperial';
+    console.log(apiUrl);
+
+    getWeather();
+};
+
+function getWeather() {
+    fetch(apiUrl)
+        .then(function(responce){
+            return responce.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.list.length; i+=7){
+                console.log(data.list[i].main.temp);
+            }
+        });
+};
